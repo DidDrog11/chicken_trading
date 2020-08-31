d0d0370214a11886cb58a8a8f497df79dd8d8de9
DidDrog11 <dsimons1987@gmail.com>
2020-08-31 17:53:08 +0100

    tidying folder

--- old_scripts/Proportional_production.R
+++
@@ -1,4 +0,0 @@
-Proportional_production_meat <- function (x, y) {
-  x %>%
-    mutate(Proportion_meat = (x$`1000 Heads`[x$Year == 1992]/y$`1000 Heads`[y$Year == 1992])*x$`1000 Heads`)
-}
--- scripts/network_analysis.R
+++
@@ -1,6 +0,0 @@
-library('tidyverse')
-source('Edge_to_Adjacent.R')
-
-heads_long <- read_csv('cleaned_data/production_heads_long.csv')
-tonnes_long <- read_csv('cleaned_data/production_tonnes_long.csv')
-
